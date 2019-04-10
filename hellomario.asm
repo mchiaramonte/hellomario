b946edcb755469c6b7d26cf111192c5c14237884
mchiaramonte <m6chiaramonte@gmail.com>
2019-04-10 19:41:39 -0400

    Initial commit of files

---
+++ hellomario.asm
@@ -0,0 +1,122 @@
+.segment "HEADER"
+.byte "NES"
+.byte $1a
+.byte $02 ; 2 * 16KB PRG ROM
+.byte $01 ; 1 * 8KB CHR ROM
+.byte %00000000 ; mapper and mirroring
+.byte $00
+.byte $00
+.byte $00
+.byte $00
+.byte $00, $00, $00, $00, $00 ; filler bytes
+.segment "ZEROPAGE" ; LSB 0 - FF
+.segment "STARTUP"
+Reset:
+    SEI ; Disables all interrupts
+    CLD ; disable decimal mode
+
+    ; Disable sound IRQ
+    LDX #$40
+    STX $4017
+
+    ; Initialize the stack register
+    LDX #$FF
+    TXS
+
+    INX ; #$FF + 1 => #$00
+
+    ; Zero out the PPU registers
+    STX $2000
+    STX $2001
+
+    STX $4010
+
+:
+    BIT $2002
+    BPL :-
+
+    TXA
+
+CLEARMEM:
+    STA $0000, X ; $0000 => $00FF
+    STA $0100, X ; $0100 => $01FF
+    STA $0300, X
+    STA $0400, X
+    STA $0500, X
+    STA $0600, X
+    STA $0700, X
+    LDA #$FF
+    STA $0200, X ; $0200 => $02FF
+    LDA #$00
+    INX
+    BNE CLEARMEM    
+; wait for vblank
+:
+    BIT $2002
+    BPL :-
+
+    LDA #$02
+    STA $4014
+    NOP
+
+    ; $3F00
+    LDA #$3F
+    STA $2006
+    LDA #$00
+    STA $2006
+
+    LDX #$00
+
+LoadPalettes:
+    LDA PaletteData, X
+    STA $2007 ; $3F00, $3F01, $3F02 => $3F1F
+    INX
+    CPX #$20
+    BNE LoadPalettes    
+
+    LDX #$00
+LoadSprites:
+    LDA SpriteData, X
+    STA $0200, X
+    INX
+    CPX #$20
+    BNE LoadSprites    
+
+; Enable interrupts
+    CLI
+
+    LDA #%10010000 ; enable NMI change background to use second chr set of tiles ($1000)
+    STA $2000
+    ; Enabling sprites and background for left-most 8 pixels
+    ; Enable sprites and background
+    LDA #%00011110
+    STA $2001
+
+Loop:
+    JMP Loop
+
+NMI:
+    LDA #$02 ; copy sprite data from $0200 => PPU memory for display
+    STA $4014
+    RTI
+
+PaletteData:
+  .byte $22,$29,$1A,$0F,$22,$36,$17,$0f,$22,$30,$21,$0f,$22,$27,$17,$0F  ;background palette data
+  .byte $22,$16,$27,$18,$22,$1A,$30,$27,$22,$16,$30,$27,$22,$0F,$36,$17  ;sprite palette data
+
+SpriteData:
+  .byte $08, $00, $00, $08
+  .byte $08, $01, $00, $10
+  .byte $10, $02, $00, $08
+  .byte $10, $03, $00, $10
+  .byte $18, $04, $00, $08
+  .byte $18, $05, $00, $10
+  .byte $20, $06, $00, $08
+  .byte $20, $07, $00, $10
+
+.segment "VECTORS"
+    .word NMI
+    .word Reset
+    ; 
+.segment "CHARS"
+    .incbin "hellomario.chr"
---
+++ make.bat
@@ -0,0 +1,2 @@
+ca65 hellomario.asm -o hellomario.o --debug-info
+ld65 hellomario.o -o hellomario.nes -t nes --dbgfile hellomario.dbgfile
